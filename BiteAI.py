29e3db14bc6b36676e1cebecd470df8716fbfaaf
DroidX <andrey.kn7@gmail.com>
2021-08-19 19:52:26 +0700

    Start

--- BiteAI.py
+++ BiteAI.py
@@ -1,4 +1,5 @@
 import json
+import Levenshtein as lev
 def long_substr(data):
     substr = ''
     if len(data) > 1 and len(data[0]) > 0:
@@ -13,30 +14,55 @@ def json_add(entry, filename):
     data.update(entry)
     with open(filename.format(1), 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False)
-book = open('food.txt', 'r', encoding='utf-8')
-r = book.read()
-bookstrings = r.split(".")
-for i in range(1,len(bookstrings),2):
-    if i + 1 < len(bookstrings):
-        data = [bookstrings[i]]
-        data.append(bookstrings[i-1])
-        key = long_substr(data)
-        print(bookstrings)
-        print(data)
-        print(long_substr(bookstrings))
-        print("Key = "+key)
-        ks0 = data[0].split(key)
-        ks1 = data[1].split(key)
-        key0 = ks1[0].strip()
-        key1 = ks0[0].strip()
-        key2 = ks1[1].strip()
-        key3 = ks0[1].strip()
-        print(key0+"==="+key1)
-        print(key2+"==="+key3)
-        filename = 'Brain.json'
-        entry = {key0: key1}
-        json_add(entry, filename)
-        entry = {key2: key3}
-        json_add(entry, filename)
+def learn_book(S):
+    book = open(S, 'r', encoding='utf-8')
+    r = book.read()
+    bookstrings = r.split(".")
+    for i in range(1,len(bookstrings),2):
+        if i + 1 < len(bookstrings):
+            data = [bookstrings[i]]
+            data.append(bookstrings[i-1])
+            key = long_substr(data)
+            print(bookstrings)
+            print(data)
+            print(long_substr(bookstrings))
+            print("Key = "+key)
+            ks0 = data[0].split(key)
+            ks1 = data[1].split(key)
+            key0 = ks1[0].strip()
+            key1 = ks0[0].strip()
+            key2 = ks1[1].strip()
+            key3 = ks0[1].strip()
+            print(key0+"==="+key1)
+            print(key2+"==="+key3)
+            filename = 'Brain.json'
+            if(key0 == "" and key3 == ""):
+                entry = {key2: key1}
+                json_add(entry, filename)
+            elif(key1 == "" and key2 == ""):
+                entry = {key0: key3}
+                json_add(entry, filename)
+            else:
+                entry = {key0: key1}
+                json_add(entry, filename)
+                entry = {key2: key3}
+                json_add(entry, filename)
+def answer(S, filename):
+    with open(filename, "r", encoding='utf-8') as file:
+        data = json.load(file)
+    akeys = data.keys()
+    mindist = 100
+    minkey = ""
+    for ii in akeys:
+        dist = lev.distance(ii,S)
+        if mindist > dist:
+            minkey = ii
+            mindist = dist
+    return(data[minkey])
+learn_book('food.txt')
+while True:
+    quest = input("> ")
+    a = answer(quest,'Brain.json')
+    print(a)
 
         
