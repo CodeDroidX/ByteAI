a08d80c426b349e31e45ba888a29e0fea454cd41
DroidX <andrey.kn7@gmail.com>
2021-08-20 16:08:23 +0700

    Brains deignore

---
+++ Brains/ActionBrain.py
@@ -0,0 +1,16 @@
+import webbrowser
+import os
+def OpenApp(q,k):
+    quest = q.split(k, 1)
+    os.system("start "+quest[1].strip())
+def Shutd():
+    os.system('shutdown /s')
+def Shutdoff():
+    os.system('shutdown /a')
+def OpenInternet(q,k):
+    quest = q.split(k, 1)
+    if len(quest[1].strip().split(".")) == 1:
+        q = quest[1].strip()+".com"
+    else:
+        q = quest[1].strip()
+    webbrowser.open('https://'+q, new=1)
